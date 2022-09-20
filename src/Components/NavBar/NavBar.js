afe38eb1cea9541fc33fec166ab72ea2790a1e97
Anya Stiefbold <2598511s@student.gla.ac.uk>
2022-09-20 09:27:15 +0100

    Basic NavBar implementation

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,14 +1,59 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
-function Navbar() {
-  return (
-    <nav>
-          <Link to="/Resume">Resume</Link>
-          <Link to="/Code">Code</Link>
-          <Link to="/Art">Art</Link>
-    </nav>
-  );
-}
 
-export default Navbar
+const NavBar = () => (
+  <Box sx={{ flexGrow: 1 }}>
+  <AppBar>
+    <Toolbar>
+      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+          Home
+        </Link>
+      </Typography>
+
+      <Typography variant="h6">
+        <Link to="/Resume" style={{ textDecoration: "none", color: "white" }}>
+          Resume
+        </Link>
+      </Typography>
+
+      <Typography variant="h6">
+        <Link to="/Code" style={{ textDecoration: "none", color: "white" }}>
+          Code
+        </Link>
+      </Typography>
+
+      <Typography variant="h6">
+        <Link to="/Art" style={{ textDecoration: "none", color: "white" }}>
+          Art
+        </Link>
+      </Typography>
+    </Toolbar>
+  </AppBar>
+</Box>
+);
+
+NavBar.propTypes = {};
+
+NavBar.defaultProps = {};
+
+
+export default NavBar;
+
+
+// function NavBar() {
+//   return (
+//     <nav>
+//           <Link to="/Resume">Resume</Link>
+//           <Link to="/Code">Code</Link>
+//           <Link to="/Art">Art</Link>
+//     </nav>
+//   );
+// }
+
+// export default NavBar
